62d65b7848cc5b0c2231d92f096589232aeba2f8
Jorge Dias <jorge@mrdias.com>
2023-11-05 20:44:39 -0400

    Automatic detection of dark mode

--- js/main.js
+++ js/main.js
@@ -1,45 +1,62 @@
+"use strict";
+
 (function () {
-  var loadCss = function(filename) {
-    var fileref = document.createElement("link");
-    fileref.setAttribute("rel", "stylesheet");
-    fileref.setAttribute("type", "text/css");
-    fileref.setAttribute("href", filename);
-    document.getElementsByTagName("head")[0].appendChild(fileref);
-  };
-  var dark = document.getElementById('dark-mode');
-  var light = document.getElementById('light-mode');
+    var loadCss = function(filename) {
+        var fileref = document.createElement("link");
+        fileref.setAttribute("rel", "stylesheet");
+        fileref.setAttribute("type", "text/css");
+        fileref.setAttribute("href", filename);
+        document.getElementsByTagName("head")[0].appendChild(fileref);
+    };
+    var dark = document.getElementsByClassName('dark-mode');
+    var light = document.getElementsByClassName('light-mode');
 
-  var hide = function(e) {
-    e.style.display = "none";
-  };
-  var show = function(e) {
-    e.style.display = "";
-  };
+    var hide = function(e) {
+        e.style.display = "none";
+    };
+    var show = function(e) {
+        e.style.display = "";
+    };
 
-  var loadDarkMode = function() {
-    loadCss('/css/dark.css');
-    setTimeout(function() {
-      var iframe = document.getElementById('twitter-widget-0');
-      iframe.contentWindow.document.getElementsByTagName('body')[0].style.color="#839496";
-    }, 500);
-    hide(dark);
-    show(light);
-  };
+    var loadDarkMode = function() {
+        for (let item of dark) {
+            show(item);
+        };
+        for (let item of light) {
+            hide(item);
+        };
+    };
 
-  dark.addEventListener("click", function(e) {
-    localStorage.setItem("mode", "dark");
-    loadDarkMode();
-  });
-  light.addEventListener("click", function(e) {
-    localStorage.setItem("mode", "light");
-    loadCss('/css/main.css');
-    hide(light);
-    show(dark);
-  });
+    var loadLightMode = function() {
+        for (let item of light) {
+            show(item);
+        };
+        for (let item of dark) {
+            hide(item);
+        };
+    };
 
-  document.addEventListener("DOMContentLoaded", function(event) {
-    if (localStorage.getItem("mode") == "dark") {
-      loadDarkMode();
+    const prefersDarkTheme = window.matchMedia('(prefers-color-scheme: dark)');
+    if (prefersDarkTheme.matches) {
+        loadDarkMode();
     }
-  });
+    prefersDarkTheme.addEventListener('change',({ matches }) => {
+        if (matches) {
+            loadDarkMode();
+        } else {
+            loadLightMode();
+        }
+    });
+
+    const prefersLightTheme = window.matchMedia('(prefers-color-scheme: light)');
+    if (prefersLightTheme.matches) {
+        loadLightMode();
+    }
+    prefersLightTheme.addEventListener('change',({ matches }) => {
+        if (matches) {
+            loadLightMode();
+        } else {
+            loadDarkMode();
+        }
+    });
 })();
